f436f5461ec256cff5f4cbb6c0c476ad3ff2ffa3
woojin0123 <iamwjwj@gmail.com>
2025-12-11 18:02:31 +0900

    251211

--- megabox/js/common.js
+++ megabox/js/common.js
@@ -1,11 +1,17 @@
 // header 2depth background
-$('.main li:not(:nth-of-type(5)').mouseenter((e)=>{
+
+// 5번째 메뉴는 제외(하위메뉴 없음)
+$('.main > li:not(:nth-of-type(5)').mouseenter(function(){
     $('#header').addClass('active');
+    $(this).find('.sub').addclass('active');
+    // $(this)는 마우스가 들어간 메인메뉴 li
 });
-$('.main li').mouseleave(()=>{
+$('.main > li').mouseleave(()=>{
+    $('#gnb .sub').removeClass('active');
+});
+$('#header .subbar').mouseenter(()=> {
     $('#header').removeClass('active');
 });
-
 // footer - looking for theater
 $(".btn_looking_theater").on("click", () => {
     $(".theater").addClass("active");
