24a35655f73b41ecdffc24cb60109f7ff2cb643c
woojin0123 <iamwjwj@gmail.com>
2025-12-23 18:02:15 +0900

    251223

---
+++ basic/react/vite/megabox-final/src/data/boxOffice.js
@@ -0,0 +1,53 @@
+// 카드 데이터
+export const datas = [
+    {
+        rank: 1,
+        poster: "/images/main/YiSbqEf6OvFcDoLoQCipDojOHqMCwKG4_420.jpg",
+        title: '주토피아',
+        screenTypes: ["/images/main/type_mega_mx4d.png",
+            "/images/main/type_dolbycinema.png",
+            "/images/main/type_dolbyatmos.png"
+        ],
+        grade: "/images/main/ALL_56x56.png",
+        age: "All",
+        like: 3699,
+        description: "더 화려해진 세계, 더 넓어진 주토피아!\n디즈니의 가~~장 사랑스러운 콤비 '주디'와 '닉'이 돌아온다!\n\n미스터리한 뱀 '게리'가 나타난 순간,",
+        score: 9.3
+    },
+    {
+        rank: 2,
+        poster: "/images/main/E9stZsj4uMvXM0YSK7wIiShsutnOtAI2_420.jpg",
+        title: "아바타",
+        screenTypes: ["/images/main/type_mega_mx4d.png",
+            "/images/main/type_dolbycinema.png",
+            "/images/main/type_dolbyatmos.png"
+        ],
+        grade: "/images/main/12_56x56.png",
+        age: "12",
+        like: 1399,
+        description: "웓드 와이드 흥행 불멸의 1위 <아바타> 시리즈의 세 번째 이야기! 판도라를 위협하는 재의 부족, 더이상 인간만이 적이 아니다!\n12월, 모두의 운명을 뒤흔들 거대한 전투가 시작된다!",
+        score: 0
+    },
+    {
+        rank: 3,
+        poster: "/images/main/fIwkU2Lnmv8AZzLk9gDbdlhDNSk2WFy6_420.jpg",
+        title: "반지의 제왕",
+        screenTypes: ["/images/main/type_mega_mx4d.png"],
+        grade: "/images/main/12_56x56.png",
+        age: "12",
+        like: 399,
+        description: "2002년 12월, 운명을 건 최후의 전쟁이 시작된다!\n9명의 반지원정대는 사우론의 사악한 세력에 맞서 반지를 지켜냈지만 반지 원정대는 뿔뿔이 흩어지게 된다.\n호빗족으로 절대 반지에 유일한 내성을 보이는 '프로도'는 일행",
+        score: 9.7
+    },
+    {
+        rank: 4,
+        poster: "/images/main/QbA0Xu2pYIIskq4gziFxbiq6eL9N2i0w_420.jpg",
+        title: "정보원",
+        screenTypes: [],
+        grade: "/images/main/15_56x56.png",
+        age: "15",
+        like: 142,
+        description: "\"나 오늘 거기 털고 옷 벗는다\"\n\n공들인 작전 실패로 강등당한 후 열정도, 의지도, 수사 감각도 모두 잃은 형사 오남혁.\n이제 그가 바라는 것은 오로지 한탕과 은퇴뿐.",
+        score: 8.5
+    },
+];
