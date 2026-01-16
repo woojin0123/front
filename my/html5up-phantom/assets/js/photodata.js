19251bce3fe39a93cc8395e0fa2edc9d8dbe5b27
woojin0123 <iamwjwj@gmail.com>
2026-01-16 18:03:05 +0900

    260116

--- my/html5up-phantom/assets/js/photodata.js
+++ my/html5up-phantom/assets/js/photodata.js
@@ -10,8 +10,8 @@ const dslr_list = [
 ];
 
 const film_list = [
-{link: "images/weverse_3-312940216.jpeg"},
-{link: "images/weverse_4-313402438.jpeg"},
+,
+{link: "images/KakaoTalk_20260112_174211224_26.jpg"},
 {link: "images/weverse_4-313402439.jpeg"},
 {link: "images/weverse_4-313817975.jpeg"},
 {link: "images/KakaoTalk_20260112_174211224_01.jpg"},
@@ -21,13 +21,21 @@ const film_list = [
 {link: "images/KakaoTalk_20260112_174211224_25.jpg"},
 {link: "images/KakaoTalk_20260112_174211224_20.jpg"},
 {link: "images/KakaoTalk_20260112_174211224_07.jpg"},
-{link: "images/KakaoTalk_20260112_174211224_26.jpg"},
-
+{link: "images/weverse_3-312940216.jpeg"}
 
 ];
 
 const md_list = [
-{link: "https://pbs.twimg.com/media/EpKefTbVoAAZiN2?format=jpg&name=small"},
+{link: "images/KakaoTalk_20260112_170600929_08.jpg"},
+{link: "images/KakaoTalk_20260112_170940600_03.jpg"},
+{link: "images/KakaoTalk_20260112_170600929_01.jpg"},
+{link: "images/KakaoTalk_20260112_170600929_03.jpg"},
+{link: "images/KakaoTalk_20260112_170940600_02.jpg"},
+{link: "images/KakaoTalk_20260112_170600929_04.jpg"},
+{link: "images/KakaoTalk_20260112_170600929_06.jpg"},
+{link: "images/KakaoTalk_20260112_170940600_02KakaoTalk_20260112_170940600_02.jpg"},
+{link: "images/KakaoTalk_20260112_171529338.jpg"},
+{link: "images/KakaoTalk_20260112_171529338_03.jpg"}
 
 ];
 const profile_list = [
